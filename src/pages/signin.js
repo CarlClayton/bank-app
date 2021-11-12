c9f59964a79aa4baee4d93e928fd354270c0c154
carlclayton <carlclayton1984@gmail.com>
2021-11-12 15:56:48 +0000

    first commit

---
+++ src/pages/signin.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ScrollToTop from "../components/ScrolToTop";
+import SignIn from "../components/Signin";
+
+const SigninPage = () => {
+  return (
+    <>
+      <ScrollToTop />
+      <SignIn />
+    </>
+  );
+};
+
+export default SigninPage;
